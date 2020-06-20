1d2e6f76c13a5f3d6d7e23320d3b5d7da167503d
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-06-20 14:45:56 +0700

    เพิ่มไฟล์ static

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors/mod.ts';
 
-import router from "./routes.ts";
+import router from './routes.ts';
 
 const port = 5000;
 
@@ -11,6 +11,13 @@ app.use(oakCors()); // Enable CORS for All Routes
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+app.use(async (context) => {
+	await context.send({
+		root: `${Deno.cwd()}/dist`,
+		index: 'index.html',
+	});
+});
 
 console.log(`Server running on port ${port}...`);
 
